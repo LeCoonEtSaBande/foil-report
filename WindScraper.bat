825c38541f7826d88e9583486831528dad78c7a9
LeCoonEtSaBande <garinlu.subs@gmail.com>
2025-07-18 14:00:14 +0200

    Update WindScraper.bat

--- WindScraper.bat
+++ WindScraper.bat
@@ -1,3 +1,6 @@
 @echo off
+echo Lancement du scraper Windguru...
+set WORKFLOW_START_TIME=%date:~-4%-%date:~3,2%-%date:~0,2% %time:~0,2%:%time:~3,2%:%time:~6,2% UTC
+set WORKFLOW_TIMEZONE=Europe/Paris
 python "Scripts_Python\windguru_csv_scraper.py"
 python "Scripts_Python\csv_to_html_viewer.py" 
