7ff904c4b065bacaab01bf9107e3a7bf2104d0d6
LeCoonEtSaBande <garinlu.subs@gmail.com>
2025-07-27 13:08:10 +0200

    Update config.py

--- Scripts_Python/config.py
+++ Scripts_Python/config.py
@@ -39,78 +39,86 @@ JS_WAIT_TIME = 2  # Temps d'attente pour le JavaScript
 # 2. Cherchez votre spot
 # 3. L'ID est dans l'URL (ex: windguru.cz/station/72305)
 #
-SITES = [
-    72305,  # Le Grand Large à Lyon
-    193,    # Chasse sur Rhône
-    2020,   # Porte les Valence
-    8248,   # Lac de Laffrey
-    314,    # Le Lac du Monteynard
-    28061,  # Le Lac du Bourget
-    179,    # Le Lac Leman
-    14     # L'Almanarre
-]
-
-# === CRITÈRES DE VENT PAR SITE ===
-#
 # Pour chaque site, définissez :
-# - "direction" : plages de directions favorables [(min1, max1), (min2, max2)]
+# - "nom" : nom qui sera affiché
+# - "direction" : plages de directions du vent favorables [(min1, max1), (min2, max2)]
 # - "vent_moyen" : seuil minimum pour considérer le vent comme "moyen"
 # - "vent_bien" : seuil pour considérer le vent comme "bon"
 # - "vent_tres_bien" : seuil pour considérer le vent comme "très bon"
-#
+# - "balise_url"
+# - "webcam_url"
+
 SITES_CRITERIA = {
-    # === SITE 72305 - Le Grand Large à Lyon ===
-    72305: {
-        "direction": [(320, 40), (140, 220)],  # Nord-Ouest à Nord-Est OU Sud-Est à Sud-Ouest
+    72305: {  # Le Grand Large à Lyon
+        "nom": "Le Grand Large à Lyon",
+        "direction": [(320, 40), (140, 220)],
         "vent_moyen": 9,
         "vent_bien": 11,
         "vent_tres_bien": 15,
-        "balise" : "https://m.winds-up.com/index.php?p=spot&id=138",
-        "webcam" : ""
+        "balise": "https://m.winds-up.com/index.php?p=spot&id=138",
+        "webcam": ""
     },
-    # === SITE 193 - Chasse sur Rhône ===
-    193: {
-        "direction": [ (140, 220)],  # Sud-Est à Sud-Ouest
+    193: {  # Chasse sur Rhône
+        "nom": "Chasse sur Rhône",
+        "direction": [(140, 220)],
         "vent_moyen": 12,
         "vent_bien": 15,
         "vent_tres_bien": 18,
-        "balise" : "",
-        "webcam" : ""
+        "balise": "",
+        "webcam": ""
     },
-        # === SITE 314 - Le Lac du Monteynard ===
-    314: {
-        "direction": [(320, 40), (140, 220)],  # Nord-Ouest à Nord-Est OU Sud-Est à Sud-Ouest
+    2020: {  # Porte les Valence
+        "nom": "Porte les Valence",
+        "direction": [],
+        "vent_moyen": 12,
+        "vent_bien": 15,
+        "vent_tres_bien": 18,
+        "balise": "",
+        "webcam": ""
+    },
+    8248: {  # Lac de Laffrey
+        "nom": "Lac de Laffrey",
+        "direction": [],
+        "vent_moyen": 10,
+        "vent_bien": 13,
+        "vent_tres_bien": 16,
+        "balise": "",
+        "webcam": ""
+    },
+    314: {  # Le Lac du Monteynard
+        "nom": "Lac du Monteynard",
+        "direction": [(320, 40), (140, 220)],
         "vent_moyen": 9,
         "vent_bien": 12,
         "vent_tres_bien": 15,
-        "balise" : "https://m.winds-up.com/index.php?p=spot&id=36",
-        "webcam" : "https://www.onekite.com/webcam-monteynard"
+        "balise": "https://m.winds-up.com/index.php?p=spot&id=36",
+        "webcam": "https://www.onekite.com/webcam-monteynard"
     },
-    # === SITE 28061 - Le Lac du Bourget ===
-    28061: {
-        "direction": [(320, 40), (140, 220)],  # Nord-Ouest à Nord-Est OU Sud-Est à Sud-Ouest
+    28061: {  # Le Lac du Bourget
+        "nom": "Lac du Bourget",
+        "direction": [(320, 40), (140, 220)],
         "vent_moyen": 12,
         "vent_bien": 15,
         "vent_tres_bien": 18,
-        "balise" : "https://balisemeteo.com/balise.php?idBalise=2456",
-        "webcam" : "https://www.skaping.com/grandlac/grandport"
+        "balise": "https://balisemeteo.com/balise.php?idBalise=2456",
+        "webcam": "https://www.skaping.com/grandlac/grandport"
     },
-    # === SITE 179 - Le Lac Leman ===
-    179: {
-        "direction": [(340, 60),(185,265)],    # Nord-Ouest à Nord-Est
-        "vent_moyen": 14,           # 14 nœuds minimum
-        "vent_bien": 17,            # 17 nœuds pour "bon"
-        "vent_tres_bien": 20,       # 20 nœuds pour "très bon"
-        "balise" : "http://meteo-sciez.fr/site/mesure_vent.php",
-        "webcam" : "https://www.skaping.com/excenevex/plage"
+    179: {  # Le Lac Léman
+        "nom": "Lac Léman",
+        "direction": [(340, 60), (185, 265)],
+        "vent_moyen": 14,
+        "vent_bien": 17,
+        "vent_tres_bien": 20,
+        "balise": "http://meteo-sciez.fr/site/mesure_vent.php",
+        "webcam": "https://www.skaping.com/excenevex/plage"
     },
-    # === SITE 14 - L'Almanarre ===
-    14: {
-        "direction": [(50, 130), (230, 310)],  # Vent d'Est ou vent d'Ouest
+    14: {  # L'Almanarre
+        "nom": "L'Almanarre",
+        "direction": [(50, 130), (230, 310)],
         "vent_moyen": 9,
         "vent_bien": 11,
         "vent_tres_bien": 15,
-        "balise" : "https://m.winds-up.com/index.php?p=spot&id=2",
-        "webcam" : "https://hyeres.fr/webcams/"
+        "balise": "https://m.winds-up.com/index.php?p=spot&id=2",
+        "webcam": "https://hyeres.fr/webcams/"
     }
 }
