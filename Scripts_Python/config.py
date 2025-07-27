1e456ff44ff7e8874c215e6a297addfe751e7fe7
LeCoonEtSaBande <garinlu.subs@gmail.com>
2025-07-27 03:39:34 +0200

    Update config.py

--- Scripts_Python/config.py
+++ Scripts_Python/config.py
@@ -28,7 +28,7 @@ FIREFOX_PATH = r"C:\Program Files\Mozilla Firefox\firefox.exe"
 
 # === Configuration du scraping ===
 HEADLESS_MODE = True  # True = mode sans interface, False = avec interface
-WAIT_TIME = 10  # Temps d'attente en secondes pour le chargement des pages
+WAIT_TIME = 8  # Temps d'attente en secondes pour le chargement des pages
 JS_WAIT_TIME = 2  # Temps d'attente pour le JavaScript
 
 # === SITES À SURVEILLER ===
